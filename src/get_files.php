aa53aba0bcd172a6108864060c34bfac8e80dfe0
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-10-02 21:31:10 +0600

    update get_files.php

--- src/get_files.php
+++ src/get_files.php
@@ -2,6 +2,10 @@
 // get_files.php
 
 $targetDir = "../assets/uploads/";
-$files = array_diff(scandir($targetDir), array('..', '.')); // Get all files except '.' and '..'
-echo json_encode(array_values($files)); // Return as JSON
+
+// Exclude '.', '..', and '.gitignore' from the file list
+$files = array_diff(scandir($targetDir), array('..', '.', '.gitignore'));
+
+// Return the list of files as JSON
+echo json_encode(array_values($files));
 ?>
